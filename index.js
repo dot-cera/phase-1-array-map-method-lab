2911e3a393b535bc3e9f1cfc52bad8638fe663af
dot-cera <ritaclaris980@gmail.com>
2024-01-09 11:53:55 +0300

    Assignment

--- index.js
+++ index.js
@@ -12,5 +12,10 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(elements => {
+    return elements
+    .split (' ')
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(' ')
+})
 }
